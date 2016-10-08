89494bb0b373a4086281dfee4cef8147d0fd5462
sheribod <sheri@sheribodnaruk.com>
2016-10-07 22:27:32 -0700

    Commit my Mod 4 Assignment

---
+++ mod4-solution/script.js
@@ -0,0 +1,12 @@
+(function () {
+  var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+      for (var i = 0; i < names.length; i++ ) {
+        var firstLetter = names[i].charAt(0);
+        firstLetter.toLowerCase();
+      if (firstLetter.toLowerCase() == "j") {
+        byeSpeaker.speak(names[i]);// byeSpeaker.xxxx
+      } else {
+        helloSpeaker.speak(names[i]);// helloSpeaker.xxxx
+    }
+}
+})();
